f2980d911a1f78394b4447ad21621115166c5692
Jatin <you@example.com>
2025-08-01 19:15:32 +0530

    Refactor user profile creation on signup to use get_or_create method

--- mgame/signals.py
+++ mgame/signals.py
@@ -4,5 +4,4 @@ from .models import Profile
 
 @receiver(user_signed_up)
 def create_user_profile(request, user, **kwargs):
-    print("🔥 SIGNAL TRIGGERED FOR USER:", user.username)  # Add this
-    Profile.objects.create(user=user)
+    Profile.objects.get_or_create(user=user)
